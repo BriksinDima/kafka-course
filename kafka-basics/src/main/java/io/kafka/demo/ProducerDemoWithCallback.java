f6fa68c5796effcce8e7341cd4646ac45f0f9fc9
Dima <briksindima@gmail.com>
2022-02-24 20:19:19 +0100

    creating kafka producer with callback and sticky partitioner

---
+++ kafka-basics/src/main/java/io/kafka/demo/ProducerDemoWithCallback.java
@@ -0,0 +1,70 @@
+package io.kafka.demo;
+
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Kafka producer");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //create a producer record
+
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Hello world " + i);
+
+            producer.send(producerRecord, (metadata, e) -> {
+                // executes every time a record successfully sent or an exception thrown
+                if (e == null) {
+                    // the record was successfully sent
+                    log.info("Received new metadata/ \n" +
+                            "Topic: " + metadata.topic() + "\n"+
+                            "Partition: " + metadata.partition() + "\n"+
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp());
+                } else {
+                    log.error("Error while producing", e);
+                }
+            });
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+        // send the data -async
+
+        // flush data - sync
+
+        producer.flush();
+
+        // flush and close producer
+
+        producer.close();
+
+
+    }
+}
