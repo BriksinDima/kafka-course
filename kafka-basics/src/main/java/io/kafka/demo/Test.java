cec7708f83980615d3b0edac3db800aaf5595246
Dima <briksindima@gmail.com>
2022-11-01 20:19:19 +0100

    Java consumer Incremental cooperative rebalance & static group membership

---
+++ kafka-basics/src/main/java/io/kafka/demo/Test.java
@@ -0,0 +1,13 @@
+package io.kafka.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+
+        if(a == 10){
+            System.out.println(1);
+        } else {
+            System.out.println(12);
+        }
+    }
+}
